fa2cde5eb1fe12acd0973556660de905f4c513ef
Tilen Krajnc <mali.tilen1@gmail.com>
2020-05-09 17:59:36 +0200

    Commit začetnih datotek
    Vsebuje začetno stran, ki prikazuje le nedelujoči login.

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Header />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <Login />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Login from './components/Login.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Login
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <b-nav>
+            <b-nav-item>Login</b-nav-item>
+            <b-nav-item>Register</b-nav-item>
+        </b-nav>
+    </div>
+</template>
