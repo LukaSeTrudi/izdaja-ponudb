fbffb455db6c564e2a944193482d679558a6ed0c
luka_pavcnik <lukapavcnik5@gmail.com>
2020-05-12 12:03:53 +0200

    asd

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace IzdajaPonudb
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1(1));
+            Application.Run(new Login());
         }
     }
 }
